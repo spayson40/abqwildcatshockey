58d62d43295d455e4f8600da5c085e9026ed2df1
Seth Payson <spayson40@gmail.com>
2025-04-20 22:41:04 -0600

    delete temp file

--- server/models/Admin_temp.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Admin', AdminSchema);
